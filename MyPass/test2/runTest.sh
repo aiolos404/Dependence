22aa6d2b34a6a4f864976fa3ecaad43f0710694a
aiolos404 <aiolos404@gmail.com>
2016-06-15 13:19:48 -0700

    clean

--- MyPass/test2/runTest.sh
+++
@@ -1,3 +0,0 @@
-#You may need to adjust the path after -load before running this script
-opt -load ../../../../Debug+Asserts/lib/MyPass.dylib -MyPass -disable-output test2.bc
-
--- MyPass/test2/test2a.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-extern void foo(float a);
-
-int main(){
-    foo(1.2f);
-    printf("Hello!\n");
-}
